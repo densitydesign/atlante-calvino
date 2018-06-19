045861d765dd61204da7fdd60a6069bab6959a2f
iosonosempreio <tommaso.elli@gmail.com>
2018-06-19 18:00:43 +0200

    groups

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var ganttSvg = d3.select("#gantt svg"),
     brushSvg = d3.select("#brush svg"),
 
-    margin = { top: 20, right: 20, bottom: 110, left: 40 },
+    margin = { top: 20, right: 20, bottom: 110, left: 200 },
     margin2 = { top: 0, right: 0, bottom: 18, left: 0 },
     width = +ganttSvg.node().getBoundingClientRect().width - margin.left - margin.right,
     height = +ganttSvg.node().getBoundingClientRect().height - margin.top - margin.bottom,
@@ -9,7 +9,7 @@ var ganttSvg = d3.select("#gantt svg"),
 
 var x = d3.scaleTime().range([0, width]),
     x2 = d3.scaleTime().range([0, width]),
-    y = d3.scaleBand().range([height, 0]),
+    y = d3.scaleBand().range([height, 0]).paddingInner(0.5),
     y2 = d3.scaleBand().range([height2, 0]);
 
 var xAxis = d3.axisBottom(x),
@@ -53,8 +53,34 @@ function brushed() {
     if (d3.event.sourceEvent && d3.event.sourceEvent.type === "zoom") return; // ignore brush-by-zoom
     var s = d3.event.selection || x2.range();
     x.domain(s.map(x2.invert, x2));
-    // focus.select(".area").attr("d", area);
+
     focus.select(".axis--x").call(xAxis);
+    focus.selectAll('.group line')
+        .attr('x1', x(x.domain()[0]))
+        .attr('x2', x(x.domain()[1]))
+
+    focus.selectAll('.stesura')
+        .attr('x', function(d) {
+            if (d.start) {
+                return x(d.start)
+            } else {
+                return 0;
+            }
+        })
+        .attr('width', function(d) {
+            let thisWidth = x(d.end) - x(d.start);
+            if (thisWidth >= 0) {
+                return thisWidth
+            } else {
+                return 0
+            }
+        })
+
+    focus.selectAll('.symbol')
+        .attr('transform', function(d) {
+            return 'translate(' + x(d.publication) + ',' + y.bandwidth() / 2 + ')';
+        })
+
     ganttSvg.select(".zoom").call(zoom.transform, d3.zoomIdentity
         .scale(width / (s[1] - s[0]))
         .translate(-s[0], 0));
@@ -70,13 +96,26 @@ function zoomed() {
 }
 
 function update(data) {
-    let yDomain = [];
+    console.log(data);
 
-    let titoli = data.info.elements.map(function(d) {
-        return d.id;
-    })
-    titoli.forEach(function(d) {
-        if (yDomain.indexOf(d) < 0) yDomain.push(d);
+    let gruppi = data.info.elements.filter(function(d) { return d.gruppo })
+
+    let pubblicazioni = d3.nest()
+        .key(function(d) { return d.pubblicazione })
+        .rollup(function(v) {
+            let newValues = []
+            v.map(function(d) {
+                if (d.tipologia != 'ibidem') {
+                    newValues.push(d);
+                }
+            })
+            return newValues
+        })
+        .entries(data.pubblicazioni.elements)
+
+    let groups = pubblicazioni.filter(function(d) {
+        let index = gruppi.map(function(e) { return e.id }).indexOf(d.key)
+        return index > -1;
     })
 
     let xDomain = [];
@@ -87,10 +126,9 @@ function update(data) {
     data.pubblicazioni.elements.forEach(function(d) {
         if (d.publication) xDomain.push(d.publication);
     })
-
     x.domain(d3.extent(xDomain));
-    y.domain(yDomain);
 
+    y.domain(groups.map(function(d) { return d.key }));
     x2.domain(x.domain());
     y2.domain(y.domain());
 
@@ -112,6 +150,96 @@ function update(data) {
         .attr("class", "brush")
         .call(brush)
         .call(brush.move, x.range());
+
+
+
+
+
+    let group = focus.selectAll('.group')
+
+    group = group.data(groups, function(d) { d.key });
+
+    group.exit().remove();
+
+    group = group.enter().append('g')
+        .attr('class', function(d) { return 'group ' + d.key })
+        .merge(group)
+        .attr('transform', function(d) { return 'translate(0,' + y(d.key) + ')' })
+        .on('click', function(d){
+          console.log(d);
+        })
+
+    group.append('line')
+        .attr('y1', y.bandwidth() / 2)
+        .attr('y2', y.bandwidth() / 2)
+        .attr('x1', x(x.domain()[0]))
+        .attr('x2', x(x.domain()[1]))
+
+    group.append('path')
+        .attr('class', 'symbol')
+        .attr("d", function(d) {
+            var symbol = d3.symbol().size(100);
+            return symbol.type(d3['symbolCross'])();
+        })
+        .attr('transform', function(d) {
+            if (!d.publication) {
+                let thisPublication = data.pubblicazioni.elements.filter(function(e) {
+                    return e.id == d.key;
+                })[0]
+                d.publication = thisPublication.publication;
+            }
+            return 'translate(' + x(d.publication) + ',' + y.bandwidth() / 2 + ')';
+        })
+
+
+
+    group.each(function(d, i) {
+
+        let stesura = d3.select(this).selectAll('rect');
+
+        let datiStesure = [];
+
+        d.value.forEach(function(e) {
+            let stesureComposizione = data.stesure.elements.filter(function(f) { return f.id == e.id; })
+            stesureComposizione.forEach(function(g) { datiStesure.push(g); })
+        })
+
+        // console.log(datiStesure)
+
+        stesura = stesura.data(datiStesure);
+
+        stesura.exit().remove();
+
+        stesura.enter().append('rect')
+            .attr('class', 'stesura')
+            .attr('height', y.bandwidth())
+            .style('opacity', function(d){
+              if (d.precision_start == 'day' && d.precision_end == 'day') {
+                return .9
+              } else {
+                return .2
+              }
+            })
+            .style('fill', '#FF5733')
+            .attr('y', 0)
+            .attr('x', function(d) {
+                if (d.start) {
+                    return x(d.start)
+                } else {
+                    return -10;
+                }
+            })
+            .attr('width', function(d) {
+                let thisWidth = x(d.end) - x(d.start);
+                if (thisWidth >= 0) {
+                    return thisWidth
+                } else {
+                    return 10
+                }
+            })
+
+    })
+
 
     // // make scrollwheel zoom unactive
     // ganttSvg.append("rect")
@@ -136,8 +264,14 @@ function init() {
                     d.precision_start = 'year';
                     if (d.start_month) {
                         d.precision_start = 'month';
+                        if (d.start_month.length < 2) {
+                            d.start_month = '0' + d.start_month
+                        }
                         if (d.start_day) {
                             d.precision_start = 'day';
+                            if (d.start_day.length < 2) {
+                                d.start_day = '0' + d.start_day
+                            }
                             var dateString = d.start_year + '-' + d.start_month + '-' + d.start_day;
                             d.start = parseDate(dateString);
                         } else {
@@ -157,8 +291,14 @@ function init() {
                     d.precision_end = 'year';
                     if (d.end_month) {
                         d.precision_end = 'month';
+                        if (d.end_month.length < 2) {
+                            d.end_month = '0' + d.end_month
+                        }
                         if (d.end_day) {
                             d.precision_end = 'day';
+                            if (d.end_day.length < 2) {
+                                d.end_day = '0' + d.end_day
+                            }
                             var dateString = d.end_year + '-' + d.end_month + '-' + d.end_day;
                             d.end = parseDate(dateString);
                         } else {
