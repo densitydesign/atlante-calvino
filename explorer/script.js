2da2eaf30f810057af143200b25fcbd01a65f663
crillion <crillion@tiscali.it>
2019-04-15 14:50:32 +0200

    atlante-calvino / explorer : partial work to catch text highlighting subdivisions

--- explorer/script.js
+++ explorer/script.js
@@ -1,4 +1,8 @@
 var text;
+var parentElement;
+var curentSelection;
+let currentSelectionStartRelative;
+let currentSelectionEndRelative;
 
 function openTextFile(event) {
   var input = event.target;
@@ -7,7 +11,7 @@ function openTextFile(event) {
     text = reader.result;
 
 //    document.getElementById('output-box').innerHTML = text;
-    document.getElementById('output-box').innerHTML = "<div id='output-block-1' data-pos=100>" + text + "</div>";
+    document.getElementById('output-box').innerHTML = "<div id='output-block-1' data-pos=0>" + text + "</div>";
 
     if (text) {
       $('#saveBtn').show()
@@ -75,21 +79,64 @@ function textSelection() {
 
   // console.log(document.getSelection().focusNode.parentElement.id)
 
-  let parentElement = document.getSelection().focusNode.parentElement;
+  parentElement = document.getSelection().focusNode.parentElement;
 
 //  if (document.getSelection().focusNode.parentElement.id.contains('output-block')) {
   if (parentElement.id.includes('output-block')) {
-    let currentSelection = document.getSelection().toString();
-    let currentSelectionStart = document.getSelection().getRangeAt(0).startOffset + (+parentElement.dataset.pos);
-    let currentSelectionEnd = document.getSelection().getRangeAt(0).endOffset + (+parentElement.dataset.pos);
+    currentSelection = document.getSelection().toString();
     d3.select('#current-selection').html(currentSelection);
-    d3.select('#current-selection-start').html(currentSelectionStart);
-    d3.select('#current-selection-end').html(currentSelectionEnd);
+
+    currentSelectionStartRelative = document.getSelection().getRangeAt(0).startOffset;
+    let currentSelectionStartAbsolute = currentSelectionStartRelative + (+parentElement.dataset.pos);
+    d3.select('#current-selection-start').html(currentSelectionStartAbsolute);
+
+    currentSelectionEndRelative = document.getSelection().getRangeAt(0).endOffset;
+    let currentSelectionEndAbsolute = currentSelectionEndRelative + (+parentElement.dataset.pos);
+    d3.select('#current-selection-end').html(currentSelectionEndAbsolute);
   }
 }
 
+function spacesToHtmlSpaces(s)
+{
+    return s.replace(" ", "&nbsp;")
+}
+
+function htmlSpacesToSpaces(s)
+{
+    return s.replace("&nbsp;", " ");
+}
+
 function addInfoClick() {
-  alert("ciao!");
+//  let innerHtml = d3.select("#output-box").nodes().map((d) => { return d.innerHTML; })
+    let innerHtml = parentElement.innerHTML;
+
+/*
+    parentElement.innerHTML = 
+        parentElement.innerHTML.substring(0, currentSelectionStartRelative) + 
+        "</div><span>" + 
+        currentSelection +
+        "</span><div>" +
+        parentElement.innerHTML.substring(currentSelectionEndRelative, parentElement.innerHTML.length);
+*/
+
+    const originalText = htmlSpacesToSpaces(parentElement.innerText);
+
+    let textBeforeSelection = originalText.substring(0, currentSelectionStartRelative);
+    let s2 = spacesToHtmlSpaces(textBeforeSelection);
+    parentElement.innerHTML = s2;
+
+    let a = 5;
+
+    let span = document.createElement('span');
+    span.innerText = spacesToHtmlSpaces(originalText.substring(currentSelectionStartRelative, currentSelectionEndRelative));
+//    parentElement.parentNode.insertBefore(span, parentElement.nextSibling);
+    parentElement.parentNode.insertBefore(span, parentElement.nextSibling);
+
+    let divAfterSelection = document.createElement('div');
+    divAfterSelection.innerText = spacesToHtmlSpaces(originalText.substring(currentSelectionEndRelative, originalText.length));
+    parentElement.insertBefore(divAfterSelection, parentElement.nextSibling);
+
+//    $(this).children(':gt('+half+')').detach().wrapAll('<ul></ul>').parent().insertAfter(this);
 }
 
 document.addEventListener('selectionchange', textSelection);
