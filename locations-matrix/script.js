a10aa95fa4080dfc61e07b96be97e3883fa9086b
iosonosempreio <tommasoelli@Tommasos-MacBook-Pro-2.local>
2019-06-28 09:13:29 +0200

    clustered nodes af first level

--- locations-matrix/script.js
+++ locations-matrix/script.js
@@ -33,8 +33,6 @@ var y = d3.scalePoint()
 	.range([0 + margin.top, h - margin.bottom]);
 
 var r = d3.scalePow().exponent(0.5)
-	.domain([0, 82])
-	.range([0, 82]);
 
 var color = d3.scaleOrdinal()
 	.domain(categories)
@@ -45,17 +43,23 @@ var nodes = [],
 
 var simulation = d3.forceSimulation(nodes)
 	// .force("charge", d3.forceManyBody().strength(-0.5))
-	.force("link", d3.forceLink(links).strength(function(d) {
-		// console.log(d)
-		return d.kind == 'same_text' ? 0 : 0.2;
-	}).distance(10).id(function(d) { return d.id; }))
-	.force("x", d3.forceX(function(d) { return d.x }).strength(function(d) {
-		return d.part_of == '' ? 0.5 : 0;
-	}))
-	.force("y", d3.forceY(function(d) { return d.y }).strength(function(d) {
-		return d.part_of == '' ? 0.5 : 0;
-	}))
-	.force("collision", d3.forceCollide(minRadius + 1).iterations(4).strength(.5))
+	.force("link", d3.forceLink(links)
+		.strength(function(d) { return d.kind == 'same_text' ? 0 : 0.2; })
+		.distance(20)
+		.id(function(d) { return d.id; })
+	)
+	.force("x", d3.forceX(function(d) { return d.x })
+		.strength(function(d) { return d.part_of == '' ? 0.7 : 0; })
+	)
+	.force("y", d3.forceY(function(d) { return d.y })
+		.strength(function(d) { return d.part_of == '' ? 0.7 : 0; })
+	)
+	.force("collision", d3.forceCollide(function(d){
+			return r(d.totalSubNodes+1)+1
+		})
+		.iterations(4)
+		.strength(.5)
+	)
 	.on("tick", ticked);
 
 function restart() {
@@ -66,10 +70,18 @@ function restart() {
 		.classed('node', true)
 		.attr("fill", function(d) { return color(d.category) })
 		.attr('stroke', function(d) { if(d.part_of) return 'black' })
-		.attr("r", minRadius)
+		.attr("r", function(d){return r(d.totalSubNodes + 1)}) // +1 means plus itself
 		.attr("cx", function(d) { return d.x })
 		.attr("cy", function(d) { return d.y })
-		.on('click', d => console.log(d))
+		.on('click', d => console.log(d.label, d.id, d))
+		.on('mouseenter', function(d){
+			node.filter(function(e){ return e.source != d.source; }).style('opacity', 0.35)
+
+		})
+		.on('mouseleave', function(d){
+			node.style('opacity', 1)
+		})
+		.call(d3.drag().on("drag", dragged))
 		.merge(node);
 
 	// Apply the general update pattern to the links.
@@ -108,16 +120,11 @@ function ticked() {
 Promise.all([
 	d3.tsv('data.tsv')
 ]).then(function(data) {
-	var locations = data[0]//.filter(function(d) { return d.year == '1972' });
-
-	locations.forEach(function(d) {
-		if(!d.id) { d.id = d['data.id'] }
-	})
+	var locations = data[0].filter(function(d) { return +d.year >= 1972 && +d.year <= 1972 });
+	locations.forEach(function(d) { if(!d.id) { d.id = d['data.id'] } })
 
 	x.domain(d3.extent(locations, function(d) { return d.year }));
 	y.domain(categories);
-
-	console.log(locations[90]);
 
 	let graph = calculateNetwork(locations);
 
@@ -127,6 +134,7 @@ Promise.all([
 })
 
 function calculateNetwork(data) {
+	console.log('calculate network')
 	var nodes = data.map(function(d) {
 		var obj = {
 			'id': d.id,
@@ -135,12 +143,66 @@ function calculateNetwork(data) {
 			'source': d['Fonte'],
 			'year': +d.year,
 			'category': d['Categoria'],
+			'totalSubNodes':0,
 			// set x and y to make nodes appera in the precise position
 			'x': x(d.year),
 			'y': y(d['Categoria'])
 		}
 		return obj
 	})
+
+	// calculate hierarchies of sub-nodes
+	let hierarchies = d3.nest()
+		.key(function(d){ return d.part_of })
+		.entries(nodes);
+
+	hierarchies.filter(function(d){ return d.key == '' })[0]
+		.values.forEach(function(d){
+			var part_of_d = hierarchies.find(function(e){ return e.key == d.id })
+			if (part_of_d) {
+				console.log('LVL 0',d.id, d.label, 'is parent of', part_of_d.values.length);
+				d.subNodes = part_of_d.values;
+				d.totalSubNodes = part_of_d.values.length
+
+				part_of_d.values.forEach(function(e){
+					var part_of_e = hierarchies.find(function(f){ return f.key == e.id })
+					if (part_of_e) {
+						console.log('LVL 1',e.id, e.label, 'is parent of', part_of_e.values.length);
+						e.subNodes = part_of_e.values
+						e.totalSubNodes = part_of_e.values.length
+						d.totalSubNodes += e.totalSubNodes
+
+						part_of_e.values.forEach(function(f){
+							var part_of_f = hierarchies.find(function(g){ return g.key == f.id})
+							if (part_of_f) {
+								console.log('LVL 2', f.id, f.label, 'is parent of', part_of_f.values.length)
+								f.subNodes = part_of_f.values
+								f.totalSubNodes = part_of_f.values.length
+								d.totalSubNodes += f.totalSubNodes
+
+								part_of_f.values.forEach(function(g){
+									var part_of_g = hierarchies.find(function(h){ return h.key == g.id })
+									if (part_of_g) {
+										console.log('LVL 3', g.id, g.label, 'is parent of', part_of_g.values.length)
+										g.subNodes = part_of_g.values
+										g.totalSubNodes = part_of_g.values.length
+										d.totalSubNodes += f.totalSubNodes
+
+									}
+								})
+							}
+						})
+					}
+				})
+			}
+		})
+
+	console.log(hierarchies);
+
+	nodes = hierarchies.filter(function(d){ return d.key == '' })[0].values;
+
+	r.domain([1,d3.max(nodes, function(d){ return d.totalSubNodes })])
+		.range([3,d3.max(nodes, function(d){ return d.totalSubNodes })])
 
 	// create the array of edges
 	var edges = []
@@ -186,9 +248,18 @@ function calculateNetwork(data) {
 				edges.push(obj);
 			})
 		})
+
 	return { 'nodes': nodes, 'edges': edges }
 }
 
 function getRandomArbitrary(min, max) {
 	return Math.random() * (max - min) + min;
 }
+
+function dragged(d) {
+    d.x = d3.event.x, d.y = d3.event.y;
+    d3.select(this).attr("cx", d.x).attr("cy", d.y);
+    link.filter(function(l) { return l.source === d; }).attr("x1", d.x).attr("y1", d.y);
+    link.filter(function(l) { return l.target === d; }).attr("x2", d.x).attr("y2", d.y);
+	simulation.alpha(1).restart();
+  }
